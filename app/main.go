c2bc235e02fc37a1bd2943ef1107968ddc6886e9
ZhangIvan1 <zhang_ivan1@163.com>
2024-04-13 07:30:26 +0000

    pass 2nd stage

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
@@ -20,11 +20,14 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+	// output, err := cmd.Output()
+	cmd.Stdout = os.Stdout
+	cmd.stderr = os.Stderr
+	err:= cmd.Start()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(string(output))
+	//fmt.Println(string(output))
 }
